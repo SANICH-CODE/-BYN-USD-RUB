33bfddea2247bf541cec18ffd757247c0d96e1a9
Sanich <115898376+SANICH-CODE@users.noreply.github.com>
2023-04-07 13:06:43 +0000

    Обновлён курс валют на дату 07 04 23, RUB снова включен.

--- .ccls-cache/@home@runner@Kalkuliator-valiut-iz-BYN-v-USD-ili-RUB/main.cpp
+++ .ccls-cache/@home@runner@Kalkuliator-valiut-iz-BYN-v-USD-ili-RUB/main.cpp
@@ -15,8 +15,25 @@ cin >> byn;
   }
 sleep (2);
 cout << getenv("*");
-//cout << "Конвертация в USD временно отключена." << endl;
-cout << "За " << byn << " BYN Вы атрымаеце " << byn / 2.8627 << " USD" << endl << "Курс заснаваны 04.04.2023" << endl; 
+//cout << endl << "Конвертация в USD временно отключена." << endl << "Код ошибки: " << getenv("error") << endl << "" << endl;
+cout << endl << "За " << byn << " BYN Вы атрымаеце " << byn / 2.9264 << " USD" << endl << "Курс заснаваны 07.04.2023" << endl << endl; 
+char* mySecret = getenv("dollar");
+cout << getenv("**");
+}
+
+void cny (){
+int byn;
+cout << "Удакладніце колькасць BYN для перакладу ў CNY (кітайскій юань)" << endl << "Колькасць BYN: ";
+cin >> byn;
+ if (byn > 1000000)
+  {
+   cout << getenv("***");
+   cout << endl << "Результат может быть некорректен!" << endl;
+  }
+sleep (2);
+cout << getenv("*");
+//cout << endl << "Конвертация в CNY временно отключена." << endl << "Код ошибки: " << getenv("error") << endl << "" << endl;
+cout << endl << "За " << byn << " BYN Вы атрымаеце " << byn / 0.42621 << " CNY" << endl << "Курс заснаваны 07.04.2023" << endl << endl; 
 char* mySecret = getenv("dollar");
 cout << getenv("**");
 }
@@ -32,8 +49,8 @@ cin >> byn;
   }
 sleep (2);
 cout << getenv("*");
-//cout << "Конвертация в EURO временно отключена." << endl;
-cout << "За " << byn << " BYN Вы атрымаеце " << byn / 3.1066 << " EUR" << endl << "Курс заснаваны 04.04.2023" << endl; 
+//cout << endl << "Конвертация в EUR временно отключена." << endl << "Код ошибки: " << getenv("error") << endl << "" << endl;
+cout << endl << "За " << byn << " BYN Вы атрымаеце " << byn / 3.1918 << " EUR" << endl << "Курс заснаваны 07.04.2023" << endl << endl; 
 char* mySecret = getenv("dollar");
 cout << getenv("**");
 }
@@ -50,15 +67,15 @@ cin >> byn;
   }
 sleep (2);
 cout << getenv("*");
-//cout << "Конвертация в RUB временно отключена." << endl;
-cout << "За " << byn << " BYN Вы атрымаеце " << byn / 0.036896 << " RUB" << endl << "Курс заснаваны 04.04.2023" << endl; 
+//cout << endl << "Конвертация в RUB временно отключена." << endl << "Код ошибки: " << getenv("off") << endl << "" << endl;
+cout << "За " << byn << " BYN Вы атрымаеце " << byn / 0.036493 << " RUB" << endl << "Курс заснаваны 07.04.2023" << endl; 
 cout << getenv("**");
 }
 
 int main() {
   int byn;
   char val1;
-  cout << "Здравствуйте, выберіце валюту (usd=u/rub=r/euro=e)\n";
+  cout << "Здравствуйте, выберіце валюту (usd=u/rub=r/euro=e/cny=c)\n";
   cout << "Валюта: ";
   cin >> val1;
 
@@ -73,6 +90,10 @@ int main() {
     if (val1 == 'e')
       {
         euro();
+      }
+    if (val1 == 'c')
+      {
+        cny();
       }
 }
 //  //
--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.clang_12
+		pkgs.haskellPackages.servant-multipart-client
+  pkgs.clang_12
 		pkgs.ccls
 		pkgs.gdb
 		pkgs.gnumake
